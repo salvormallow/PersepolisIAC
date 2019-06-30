69d334434515e5920f01d14396cd2f2716df1eb8
Unknown <unknown@example.com>
2019-06-30 01:30:55 -0700

    Things working

--- Persepolis.tf
+++ Persepolis.tf
@@ -2,20 +2,145 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_launch_template" "foobar" {
-  name_prefix   = "persepolis"
-  image_id      = "ami-1a2b3c"
-  instance_type = "t2.micro"
+#VPC resources
+resource "aws_vpc" "persepolis" {
+  cidr_block       = "10.0.0.0/16"
+}
+resource "aws_subnet" "subnetA" {
+  vpc_id     = "${aws_vpc.persepolis.id}"
+  cidr_block = "10.0.0.0/24"
+  availability_zone = "us-east-1a"
+  map_public_ip_on_launch = true
 }
 
-resource "aws_autoscaling_group" "bar" {
-  availability_zones = ["us-east-1a"]
-  desired_capacity   = 1
-  max_size           = 1
-  min_size           = 1
+resource "aws_subnet" "subnetB" {
+  vpc_id     = "${aws_vpc.persepolis.id}"
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-1b"
+  map_public_ip_on_launch = true
+}
 
-  launch_template {
-    id      = "${aws_launch_template.foobar.id}"
-    version = "$Latest"
+resource "aws_subnet" "subnetC" {
+  vpc_id     = "${aws_vpc.persepolis.id}"
+  cidr_block = "10.0.2.0/24"
+  availability_zone = "us-east-1c"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = "${aws_vpc.persepolis.id}"
+}
+
+resource "aws_route" "r" {
+  route_table_id            = "${aws_vpc.persepolis.main_route_table_id}"
+  destination_cidr_block    = "0.0.0.0/0"
+  gateway_id = "${aws_internet_gateway.gw.id}"
+}
+
+#
+//resource "aws_cloudformation_stack" "backend" {
+//  name = "persepolis"
+//
+//  parameters = {
+//    VPCCidr = "10.0.0.0/16"
+//  }
+//
+//  template_body = "${file("Persepolis.cform")}"
+//}
+
+#Autoscaling group
+
+resource "aws_iam_instance_profile" "persepolis_profile" {
+  name = "persepolis_profile"
+  role = "${aws_iam_role.persepolis_role.name}"
+}
+
+resource "aws_iam_role" "persepolis_role" {
+  name = "persepolis_role"
+  path = "/"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": {
+    "Effect": "Allow",
+    "Principal": {"Service": "ec2.amazonaws.com"},
+    "Action": "sts:AssumeRole"
   }
 }
+EOF
+}
+
+resource "aws_iam_role_policy" "persepolis_policy" {
+  name = "persepolis_policy"
+  role = "${aws_iam_role.persepolis_role.id}"
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": "firehose:*",
+            "Resource": "arn:aws:firehose:*"
+        }
+    ]
+}
+EOF
+}
+
+
+resource "aws_key_pair" "persepolis-key" {
+  key_name   = "persepolis-key"
+  public_key = "${file("persepolis-key.pub")}"
+}
+
+resource "aws_launch_configuration" "persepolis_launch" {
+  name_prefix   = "persepolis-"
+  image_id      = "ami-02eac2c0129f6376b"
+  instance_type = "t2.micro"
+  key_name = "${aws_key_pair.persepolis-key.key_name}"
+  user_data = "${file("persepolis-datapump.sh")}"
+  security_groups = ["${aws_security_group.allow_ssh.id}"]
+  iam_instance_profile = "${aws_iam_instance_profile.persepolis_profile.id}"
+
+  root_block_device {
+    delete_on_termination = true
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_autoscaling_group" "persepolis_autoscale" {
+  name                 = "persepolis_autoscale"
+  launch_configuration = "${aws_launch_configuration.persepolis_launch.name}"
+  min_size             = 3
+  max_size             = 5
+  vpc_zone_identifier  = ["${aws_subnet.subnetA.id}", "${aws_subnet.subnetB.id}", "${aws_subnet.subnetC.id}"]
+  depends_on = ["aws_internet_gateway.gw"]
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+  vpc_id      = "${aws_vpc.persepolis.id}"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
---
+++ persepolis-datapump.sh
@@ -0,0 +1,97 @@
+#! /bin/bash
+sudo yum update -y
+
+#install AWSCLI
+sudo yum install -y awscli
+
+#install and configure nginx
+sudo yum install epel-release -y
+sudo yum install -y nginx
+sudo systemctl start nginx
+sudo systemctl enable nginx
+chmod -R og+rx /var/log/nginx /var/log/messages
+
+#install and configure fluentD
+curl -L https://toolbelt.treasuredata.com/sh/install-redhat-td-agent3.sh | sh
+sudo -u td-agent IP="$(curl http://169.254.169.254/latest/meta-data/public-ipv4)"
+sudo td-agent-gem install fluent-plugin-kinesis
+sudo echo '#source section
+<source>
+  @type tail
+  path /var/log/nginx/access.log
+  pos_file /var/log/td-agent/nginx.access_log.pos
+  <parse>
+    @type nginx
+  </parse>
+  tag nginx
+</source>
+
+<source>
+  @type tail
+  path /var/log/messages
+  pos_file /var/log/td-agent/messages.pos
+  read_from_head true
+  <parse>
+    @type none
+  </parse>
+  tag messages
+</source>
+
+#match section
+
+<match nginx>
+  @type kinesis_firehose
+  region us-west-1
+  delivery_stream_name PersepolisCollector
+</match>
+
+<match messages>
+  @type kinesis_firehose
+  region us-west-1
+  delivery_stream_name PersepolisCollector
+</match>
+
+#filter section
+
+<filter **>
+	@type record_modifier
+	<record>
+		hostname "#{Socket.gethostname}"
+		tag ${tag}
+		timestamp ${Time.at(time).to_s}
+		ip "#{ENV[\'FOO\']}"
+	</record>
+</filter>
+
+#defaults
+
+<match td.*.*>
+  @type tdlog
+  @id output_td
+  apikey YOUR_API_KEY
+
+  auto_create_table
+  <buffer>
+    @type file
+    path /var/log/td-agent/buffer/td
+  </buffer>
+
+  <secondary>
+    @type file
+    path /var/log/td-agent/failed_records
+  </secondary>
+</match>
+
+<match debug.**>
+  @type stdout
+  @id output_stdout
+</match>
+
+<source>
+  @type debug_agent
+  @id input_debug_agent
+  bind 127.0.0.1
+  port 24230
+</source>' > /etc/td-agent/td-agent.conf
+sudo systemctl start td-agent
+sudo systemctl enable td-agent
